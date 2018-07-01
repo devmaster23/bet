a12c205e4f25a61078b37fc7005d0fb2d6ebfa01
devmaster23 <ryanbrucen1023@outlook.com>
2018-07-01 11:49:19 -0400

    library update

--- application/libraries/AuthLibrary.php
+++ application/libraries/AuthLibrary.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class AuthLibrary {
+class Authlibrary {
 
     /**
      * Mark a user as logged in and create autologin cookie if wanted
