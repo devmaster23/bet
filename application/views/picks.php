fc518ca37a73bbc17cd75eef95af02384f2593ad
devmaster23 <ryanbrucen1023@outlook.com>
2018-07-06 10:36:46 -0400

    update betday selection in picks

--- application/views/picks.php
+++ application/views/picks.php
@@ -18,11 +18,13 @@
     <div class="header-div">
         <div class="game-week-select-div">
             <label>Bet Day</label>
+            <i class="fa fa-chevron-left bet-week-prev"></i>
             <select class="select2 game-week-select" name="game-week-select" onchange="initPage()">
                 <?php for($i=1; $i<=60; $i++) {?>
                 <option <?php if($i == $betweek) echo "selected";?> value="<?php echo $i?>"><?php echo $i?></option>
                 <?php }?>
             </select>
+            <i class="fa fa-chevron-right bet-week-next"></i>
         </div>
         <div class="save-button-div">
             <button type="button" class="btn btn-success btn-lg enter-pick_save-button" onClick="updateTable()">Update</button>
